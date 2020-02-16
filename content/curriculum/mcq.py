be76b1036015e6b19f1cc7d221a7ea7e9af9d186
Patrick Young <paddy10tellys@gmail.com>
2020-02-16 13:27:26 +0000

    tidied up unwanted widgets

--- content/curriculum/mcq.py
+++ content/curriculum/mcq.py
@@ -1,13 +1,15 @@
-mcq=[['The amount of gas exhaled after a maximal inpiration followed by a maximal expiration is the:',['IC','RV','VC','TV'],'VC'],
+mcq=[['The amount of gas exhaled after a maximal inpiration followed by a maximal expiration is the:',['IC','RV','VC','TV'],'VC'], 'Correct!', 'Try again...'
 ['The amount of gas in the lungs after a maximal inspiration is the:',['IC','IRV','TLC'],'TLC'],
 ['Where does gas exchange occur?',['pulmonary artery','bronchioles','alveoli', 'pleura'],'alveoli'],
 ['The pleura that surrounds the lungs consists of two layers, the ________.', ['visceral and parietal pleurae', 'mediastinal and parietal pleurae', 'visceral and mediastinal pleurae', 'none of the above'], 'visceral and parietal pleurae']]
 
-for l in mcq:
-    stem=l[0]
-    print(stem)
-    distractors=l[1]
-    for d in distractors:
-        print(d)
-    key=l[2]
+# for l in mcq:
+#     stem=l[0]
+#     print(stem)
+#     distractors=l[1]
+#     for d in distractors:
+#         print(d)
+#     key=l[2]
     # print(stem)
+
+print(mcq[0][0][4])
